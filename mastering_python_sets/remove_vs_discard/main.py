9dd0759f8a9d68ebc879243ae390cf08ed4b2f18
sybabe <>
2026-04-27 18:39:32 +0000

    remove vs discard

---
+++ mastering_python_sets/remove_vs_discard/main.py
@@ -0,0 +1,16 @@
+marvel_movies = {
+    'Avengers: Endgame',
+    'Black Panther',
+    'Iron Man',
+    'The Dark Knight',
+    'Spider-Man: No Way Home',
+    'Guardians of the Galaxy',
+    'Justice League'
+}
+
+# Write your code here
+marvel_movies.remove("The Dark Knight")
+marvel_movies.discard("Justice League")
+
+# Testing
+print("Updated set:", marvel_movies)
